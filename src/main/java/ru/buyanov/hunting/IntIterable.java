dd355804dd145e28bc67d6471aa753e895b7e48b
PiLiGRiM <piligrim18@gmail.com>
2016-02-05 13:21:52 +0300

    Update IntIterable.java

--- src/main/java/ru/buyanov/hunting/IntIterable.java
+++ src/main/java/ru/buyanov/hunting/IntIterable.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 public class IntIterable implements Iterable<Integer> {
     int[] backed;
 
+    int index = 0;
 
     public IntIterable(int[] backed) {
         this.backed = backed;
@@ -21,12 +22,12 @@ public class IntIterable implements Iterable<Integer> {
 
         public boolean hasNext() {
             //TODO: You task is implement this method
-            return false;
+            return index < backed.length;
         }
 
         public Integer next() {
             //TODO: You task is implement this method
-            return null;
+            return backed[index++];
         }
 
         public void remove() {
